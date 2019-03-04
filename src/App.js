6a6baa6f35e7eb9049f6b78ba401749b42603ab6
Jeanne <jeanneu@xello.world>
2019-03-03 23:13:48 -0500

    Add movie component to render a mock list of movies

--- src/App.js
+++ src/App.js
@@ -2,27 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Movie from './Movie';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars'
+  }, {
+    id: 2,
+    title: 'Spider Man'
+  }, {
+    id: 3,
+    title: 'Super Girl'
+  }, {
+    id: 4,
+    title: 'Super Man'
+  }
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        {movies.map( movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
 }
+
+
 
 export default App;
